daa3be307a956a20fbcf8ec2d09cff17a354f066
Ryo Murakami <integral14@gmail.com>
2019-06-15 23:59:41 +0900

    ping pong

---
+++ Assets/Scripts/PingPong.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingPong : MonoBehaviour
+{
+    public bool IsConnected;
+    public int PingPongInterval = 990;  // milliseconds - has be less than 1000???
+    
+    private RunThrottle PingPongThrottle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.PingPongThrottle = new RunThrottle(PingPongInterval);
+        this.IsConnected = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.PingPongThrottle.Run(() => {
+            try {
+                PanzerCommandSender.PingPong();
+                this.IsConnected = true;
+            } catch (System.Exception e) {
+                Debug.LogException(e);
+                this.IsConnected = false;
+            }
+            Debug.Log("ping pong throttle: " + this.IsConnected);
+        });
+    }
+}
