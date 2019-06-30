76308cb77c8bc38e99983c2b2e467d7bac2d88f4
Ryo Murakami <integral14@gmail.com>
2019-06-30 11:58:23 +0900

    engine sound

---
+++ Assets/Scripts/Engine.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engine : MonoBehaviour
+{
+    AudioSource AudioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.AudioSource = GetComponent<AudioSource>();
+        this.AudioSource.volume = 0.5f;
+
+        EventBus.Instance.Subscribe(OnController);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnController(float leftLevel, float rightLevel)
+    {
+        // in [0, 1]
+        float level = Mathf.Max(Mathf.Abs(leftLevel), Mathf.Abs(rightLevel));   
+
+        this.AudioSource.volume = 0.5f + level / 2;
+    }
+}
