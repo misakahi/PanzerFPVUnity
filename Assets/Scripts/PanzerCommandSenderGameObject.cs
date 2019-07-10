e4bf64d47efedb2535c094e98194285fd2eec1c5
Ryo Murakami <integral14@gmail.com>
2019-07-11 00:18:15 +0900

    command sender game object

---
+++ Assets/Scripts/PanzerCommandSenderGameObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanzerCommandSenderGameObject : MonoBehaviour
+{
+    public string host = "localhost";
+    public int port = 50051;
+    private PanzerCommandSender panzerCommandSender;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.panzerCommandSender = PanzerCommandSender.withConnection(host, port);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
